bcbcf75e4417e375011f39021bd74f9a6e612aa0
Abu shama <mabu53140@gmail.com>
2024-11-23 13:18:14 +0000

    operator

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -37,3 +37,48 @@ let someNumber = 33;
 // let stringNumber = String(someNumber);
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// ==== Why string to number conversion is confusing ===//
+
+// ==== Operations =====//
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(5 + 5);
+// console.log(5 - 5);
+// console.log(5 * 5);
+// console.log(5 ** 5);
+// console.log(5 / 5);
+// console.log(5 % 5);
+
+let str1 = 'hello';
+let str2 = " abu"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 5 * 6 % 3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+true+);
+
+// console.log(+"");
+
+// let num1 , num2 , num3
+// num1 = num2 = num3 = 67;
+// console.log(num1 , num2 , num3);
+
+let gameCounter = 100;
+// gameCounter++;
+// ++gameCounter;
+// gameCounter--
+// --gameCounter
+console.log(gameCounter);
