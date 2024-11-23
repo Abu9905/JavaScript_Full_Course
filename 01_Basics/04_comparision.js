6f5c3599d58ff9648260afffc144da184305c78d
Abu shama <mabu53140@gmail.com>
2024-11-23 13:29:35 +0000

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,28 @@
+
+// ===Comparison of dataTypes ===//
+// console.log(8 > 9);
+// console.log(8 >= 9);
+// console.log(8 < 9);
+// console.log(8 <= 9);
+// console.log(8 != 9);
+// console.log(8 !== 9);
+// console.log(9 == 9);
+// console.log(9 === 9);
+
+// console.log('2' > 1);
+// console.log('02' > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined < 0);
+// console.log(undefined <= 0);
+
+// stick check:
+// ===
+
+console.log("3" == 3);
+console.log("3" === 3);
