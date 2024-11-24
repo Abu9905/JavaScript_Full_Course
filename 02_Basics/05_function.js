5d4373b2e6126bcd3a4871cee719e362015e74da
Abu shama <mabu53140@gmail.com>
2024-11-24 19:13:11 +0000

    function

---
+++ 02_Basics/05_function.js
@@ -0,0 +1,30 @@
+// ====== Function =======//
+
+// console.log('A');
+// console.log('b');
+// console.log('u');
+// console.log('s');
+// console.log('h');
+// console.log('a');
+// console.log('m');
+// console.log('a');
+
+// function defation:
+function sayName(){
+console.log('A');
+console.log('b');
+console.log('u');
+console.log('s');
+console.log('h');
+console.log('a');
+console.log('m');
+console.log('a');
+}
+// call function
+// sayName()
+
+function addTwoNum(num1 , num2){
+    return num1 + num2;
+}
+const myNum = addTwoNum(8 , 9)
+console.log(myNum);
