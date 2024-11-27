77f477e924580e6ce41e68fc48cc066b4bd7e67e
Abu shama <mabu53140@gmail.com>
2024-11-27 02:23:41 +0000

    loop

---
+++ 06_Arrays_loops/fourth.js
@@ -0,0 +1,10 @@
+// ========= Filter , Map  and javaScript =======//
+
+const coding = ["js", "ruby", "java", "python", "cpp"];
+
+const values = coding.forEach( (item) =>{
+    // console.log(item);
+    return item
+})
+console.log(values);
+// Note :- Ye value return nahi karta hai.
